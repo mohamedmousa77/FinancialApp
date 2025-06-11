4645b74256ddb4307f7db1278f884d7dbe3ebc0f
Mohamed Mousa <77318165+mohamedmousa77@users.noreply.github.com>
2025-06-11 11:47:03 +0200

    Initial Angular project setup

---
+++ src/app/pages/pots/pots.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pots',
+  imports: [],
+  templateUrl: './pots.component.html',
+  styleUrl: './pots.component.scss'
+})
+export class PotsComponent {
+
+}
